e235eac0d5cfa51356b4e1a7e4782762f75dd779
Marvin Böddeker <marvin.boeddeker@gmail.com>
2017-03-29 12:01:13 +0200

    Initial Commit

---
+++ InstagramRebuild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramRebuild
+//
+//  Created by Marvin Böddeker on 29.03.17.
+//  Copyright © 2017 Marvin Böddeker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
